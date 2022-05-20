00797ddc61ce42d245b101ada91216bf23aa8cd3
intrvertmichael <michaelpaguay@gmail.com>
2022-05-20 05:38:20 -0400

    cleaned up api

--- pages/api/students.js
+++ pages/api/students.js
@@ -4,24 +4,26 @@ export default async function handler(req, res) {
 	console.log('req.method = ', req.method)
 	console.log('req.body = ', req.body)
 
+	// -> GET
+	let text = 'select * from students'
+	let values = []
+
 	if (req.method === 'POST') {
-		await pg.query(
-			'insert into students (first_name, last_name, age, grade) values ($1, $2, $3, $4)',
-			[req.body.f_name, req.body.l_name, req.body.age, req.body.grade],
-		)
+		text = 'insert into students (first_name, last_name, age, grade) values ($1, $2, $3, $4)'
+		values = [req.body.f_name, req.body.l_name, req.body.age, req.body.grade]
 	}
 	else if (req.method === 'PUT') {
-		console.log(req.body.field, req.body.value, req.body.id)
-		await pg.query(
-			`UPDATE students SET ${req.body.field} = $1 WHERE id = $2`,
-			[req.body.value, req.body.id]
-		)
+		text = `UPDATE students SET ${req.body.field} = $1 WHERE id = $2`
+		values = [req.body.value, req.body.id]
 	}
 	else if (req.method === 'DELETE') {
-		await pg.query('delete from students where id=$1', [req.body.id])
+		text = 'delete from students where id=$1'
+		values = [req.body.id]
 	} else {
-		// Handle any other HTTP method
+		
 	}
+	
+	await pg.query( text, values)
 
 	const updatedStudentsList = await pg.query('select * from students')
 	res.status(200).json({ students: updatedStudentsList.rows })
