09fe36504f9684930b0c798daf14c87e14fa350a
intrvertmichael <michaelpaguay@gmail.com>
2022-05-20 05:58:33 -0400

    made table into grid

--- components/students/add_input.js
+++ components/students/add_input.js
@@ -1,55 +1,61 @@
 import { useEffect, useState, useRef } from 'react'
 
-const AddInput = ({id, field, text, setStudents}) => {
-    const [input, setInput] = useState(text)
-    const [editing, setEditing] = useState(false)
-    const inputReference = useRef(null);
+const AddInput = ({ id, field, text, setStudents }) => {
+	const [input, setInput] = useState(text)
+	const [editing, setEditing] = useState(false)
+	const inputReference = useRef(null)
 
-    useEffect(() => {
-        if(inputReference.current){
-            inputReference.current.focus();
-        }
-    }, [editing]);
+	useEffect(() => {
+		if (inputReference.current) {
+			inputReference.current.focus()
+		}
+	}, [editing])
 
-    async function editStudent() {
+	async function editStudent() {
 		const res = await fetch('./api/students', {
 			method: 'PUT',
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({ 
-                id: id,
-                field: field,
-                value: input,
-            }),
+			body: JSON.stringify({
+				id: id,
+				field: field,
+				value: input,
+			}),
 		})
 		const data = await res.json()
 		setStudents(data.students)
 	}
 
-    async function formSubmitted(e){
-        e.preventDefault()
-        await editStudent()
-        setEditing(false)
-    }
+	async function formSubmitted(e) {
+		e.preventDefault()
+		await editStudent()
+		setEditing(false)
+	}
 
-    if(editing){
-        return (
-            <th className="flex justify-center bg-gray-100 border-2">
-                <form onSubmit={formSubmitted}>
-                    <input type='text' value={input} onChange={e=>setInput(e.target.value)} ref={inputReference} />
-                </form>
-                <button
-                    onClick={()=>setEditing(false)}
-                    className='px-2 bg-white'
-                >X</button>
-            </th>
-        )
-    }
+	if (editing) {
+		return (
+			<div className='flex bg-gray-100 border-2 border-r-0'>
+				<form onSubmit={formSubmitted}>
+					<input
+						type='text'
+						value={input}
+						onChange={e => setInput(e.target.value)}
+						ref={inputReference}
+					/>
+				</form>
+				<button onClick={() => setEditing(false)} className='px-2 bg-white'>
+					X
+				</button>
+			</div>
+		)
+	}
 
-    return (
-        <th className="border-2 " onClick={()=>setEditing(true)}>{text}</th>
-    )
+	return (
+		<div className='border-2 border-r-0' onClick={() => setEditing(true)}>
+			{text}
+		</div>
+	)
 }
 
 export default AddInput
--- components/students/table.js
+++ components/students/table.js
@@ -1,4 +1,4 @@
-import AddInput from "./add_input"
+import AddInput from './add_input'
 
 const StudentsTable = ({ students, setStudents }) => {
 	async function deleteStudent(id) {
@@ -18,60 +18,58 @@ const StudentsTable = ({ students, setStudents }) => {
 	return (
 		<div className='p-6 bg-white rounded'>
 			<h3 className='font-bold'>All Students</h3>
-			<table className='w-full text-sm'>
-				<thead>
-					<tr className='p-3 text-bold'>
-						<th className="border-2 ">first name</th>
-						<th className="border-2 ">last name</th>
-						<th className="border-2 ">age</th>
-						<th className="border-2 ">grade</th>
-						<th className="border-2 ">delete</th>
-					</tr>
-				</thead>
+			<div className='grid w-full grid-cols-5 text-sm'>
+				<div className='bg-gray-100 border-2 border-b-0 border-r-0'>
+					first name
+				</div>
+				<div className='bg-gray-100 border-2 border-b-0 border-r-0'>
+					last name
+				</div>
+				<div className='bg-gray-100 border-2 border-b-0 border-r-0'>age</div>
+				<div className='bg-gray-100 border-2 border-b-0 border-r-0'>grade</div>
+				<div className='bg-gray-100 border-2 border-b-0'>delete</div>
 
-				<tbody>
-					{students.map(student => (
-						<tr key={student.id} className=''>
-							<AddInput
-								id={student.id}
-								field='first_name'
-								text={student.first_name}
-								setStudents={setStudents}
-								/>
-							
-							<AddInput
-								id={student.id}
-								field='last_name'
-								text={student.last_name}
-								setStudents={setStudents}
-								/>
-							
-							<AddInput
-								id={student.id}
-								field='age'
-								text={student.age}
-								setStudents={setStudents}
-								/>
-							
-							<AddInput
-								id={student.id}
-								field='grade'
-								text={student.grade}
-								setStudents={setStudents}
-							/>
-							
-							<th className="border-2 ">
-								<button
-									onClick={() => deleteStudent(student.id)}
-									className='font-bold text-red-600'
-								>
-									X
-								</button>
-							</th>
-						</tr>
-					))}
-				</tbody>
-			</table>
+				{students.map(student => (
+					<>
+						<AddInput
+							id={student.id}
+							field='first_name'
+							text={student.first_name}
+							setStudents={setStudents}
+						/>
+
+						<AddInput
+							id={student.id}
+							field='last_name'
+							text={student.last_name}
+							setStudents={setStudents}
+						/>
+
+						<AddInput
+							id={student.id}
+							field='age'
+							text={student.age}
+							setStudents={setStudents}
+						/>
+
+						<AddInput
+							id={student.id}
+							field='grade'
+							text={student.grade}
+							setStudents={setStudents}
+						/>
+
+						<div className='border-2'>
+							<button
+								onClick={() => deleteStudent(student.id)}
+								className='font-bold text-red-600'
+							>
+								X
+							</button>
+						</div>
+					</>
+				))}
+			</div>
 		</div>
 	)
 }
