95a9b0de2114a46e9587b261619a1062fed4e2c3
intrvertmichael <michaelpaguay@gmail.com>
2022-06-02 20:09:26 -0400

    changed connection type to db

--- db.js
+++ db.js
@@ -1,12 +1,22 @@
 import { Pool } from 'pg'
+const env = process.env.NODE_ENV || 'development'
 
-const pg = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_DB,
-})
+if (env === 'development') {
+	connectionString = {
+		user: process.env.DB_USER,
+		password: process.env.DB_PASS,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		database: process.env.DB_DB,
+	}
+} else {
+	connectionString = {
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
+	}
+}
+
+const pg = new Pool(connectionString)
 
 pg.connect(() => {
 	console.log('connected to db')
